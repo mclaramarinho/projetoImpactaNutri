0f964acf37de4ad8b3447736f1aa5855f14a76d2
mclaramarinho <marinho.claramb@gmail.com>
2024-04-19 15:54:58 -0300

    fix: was not adding new items, only replacing existing ones. Now it's ok.

--- utils/updateDB.js
+++ utils/updateDB.js
@@ -2,25 +2,27 @@ import fs from "fs";
 
 
 export default function updateDB(pathArquivo, attrParaComparar, attrParaCompararDoDado, objetoNovo){
-
     try{
         let arrData = fs.readFileSync(pathArquivo, "ascii");
 
         arrData = JSON.parse(arrData);
+        
+        let newData = [];
 
         if(arrData.length > 0){
-            arrData = arrData.map(item => {
-                if(item[attrParaComparar] === attrParaCompararDoDado){
-                    return objetoNovo.get();
-                }else{
-                    return item;
-                }
-            })
+            newData = arrData.map(item => (item[attrParaComparar].toString() === attrParaCompararDoDado) ?
+                                            objetoNovo.get() : item);
+
+            const addedNewPerson = newData.filter(item => item[attrParaComparar].toString() === attrParaCompararDoDado);
+
+            if(addedNewPerson.length === 0){
+                newData.push(objetoNovo.get());
+            }
         }else{
-            arrData.push(objetoNovo.get())
+            newData.push(objetoNovo.get())
         }
 
-        fs.writeFileSync(pathArquivo, JSON.stringify(arrData));
+        fs.writeFileSync(pathArquivo, JSON.stringify(newData));
 
         return "ok"
 
